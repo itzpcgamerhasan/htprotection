d46c5d11cfd255a8c1325a37414dfbc9400bace9
itzpcgamerhasan <140355843+itzpcgamerhasan@users.noreply.github.com>
2025-01-11 19:47:36 +0600

    Update htcsspt.js

--- htcsspt.js
+++ htcsspt.js
@@ -1,12 +1,19 @@
 (function() {
-  var originalCode = document.querySelector('.footer-credit').innerHTML;
+  // The original footer content you want to protect
+  var originalFooterCode = "<div class='footer-credit'>Designed and Developed by <a href='#' target='_blank'>Mahmuddd Hasan</a>.<br/><a href='mailto:kingmahmudhasansamir@gmail.com'>Contact Now</a> for design inquiries.</div>";
+
+  // Get the footer element
   var footerElement = document.querySelector('.footer-credit');
-  
-  // Set the original footer content as a check
-  footerElement.addEventListener('DOMSubtreeModified', function() {
-    if (footerElement.innerHTML !== originalCode) {
-      // If the code is modified, redirect to your website
+
+  // Function to check if the footer content is modified
+  function checkFooterModification() {
+    if (footerElement.innerHTML !== originalFooterCode) {
+      // If the content is modified, redirect to the desired website
       window.location.href = "https://hasantechnologyofficial.blogspot.com/";
     }
-  });
+  }
+
+  // Listen for changes in the footer content
+  footerElement.addEventListener('DOMSubtreeModified', checkFooterModification);
+
 })();
