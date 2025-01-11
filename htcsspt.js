75b0003e849bf7f3a8a87cd23e545a66eaadc46f
itzpcgamerhasan <140355843+itzpcgamerhasan@users.noreply.github.com>
2025-01-11 20:06:35 +0600

    Update htcsspt.js

--- htcsspt.js
+++ htcsspt.js
@@ -1,19 +1,30 @@
 (function() {
-  // The original footer content you want to protect
+  // Original footer content that should not be changed
   var originalFooterCode = "<div class='footer-credit'>Designed and Developed by <a href='#' target='_blank'>Mahmuddd Hasan</a>.<br/><a href='mailto:kingmahmudhasansamir@gmail.com'>Contact Now</a> for design inquiries.</div>";
 
-  // Get the footer element
+  // Select the footer element
   var footerElement = document.querySelector('.footer-credit');
 
-  // Function to check if the footer content is modified
-  function checkFooterModification() {
+  if (!footerElement) return;
+
+  // Function to check if footer content matches the original code
+  function checkFooterContent() {
     if (footerElement.innerHTML !== originalFooterCode) {
-      // If the content is modified, redirect to the desired website
+      // If the footer content doesn't match, redirect to the original website
       window.location.href = "https://hasantechnologyofficial.blogspot.com/";
     }
   }
 
-  // Listen for changes in the footer content
-  footerElement.addEventListener('DOMSubtreeModified', checkFooterModification);
+  // Create a MutationObserver to watch for changes in the footer
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      checkFooterContent();
+    });
+  });
 
+  // Observer configuration
+  var config = { childList: true, subtree: true };
+  
+  // Start observing changes
+  observer.observe(footerElement, config);
 })();
